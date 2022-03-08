4237d71327b160274df4b48dd9b2a853fcc5d2d1
NGUIMBI CHRISMA <chrisma@macbook-pro-de-nguimbi.home>
2022-03-08 21:16:50 +0100

    exo du 8 mars

---
+++ index.js
@@ -0,0 +1,20 @@
+let age = 38;
+let name = "Nguimbi Chrisma";
+let student = false;
+let school = "Wild Code School";
+
+typeof(age);
+typeof(name);
+typeof(student);
+typeof(school);
+
+console.log(age);
+console.log(name);
+console.log(student);
+console.log(school);
+
+age = age++;
+student = true;
+
+console.log(age);
+console.log(student);
